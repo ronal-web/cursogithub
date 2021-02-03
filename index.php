8265ad0bd53d73807b899a8e94b95e8a29b5231a
ronal-web <64571434+ronal-web@users.noreply.github.com>
2021-02-02 21:56:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="css/estilos.css" rel="stylesheet" type="text/css">
-    <h1>hola</h1>
+    <h1>hola ronal</h1>
 </head>
 <body>
     
